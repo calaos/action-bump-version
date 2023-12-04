80579938e7ecae421b88b09a6bb2be8f655e162e
raoulh <raoul.hecky@gmail.com>
2023-12-04 18:07:21 +0100

    Calaos bump version action

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM alpine:latest
 
-RUN apk add --no-cache git grep
+RUN apk add --update --no-cache git grep python3 py3-semver py3-gitpython
 
 COPY entrypoint.sh /entrypoint.sh
+COPY get_last_tag.py /get_last_tag.py
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,25 +8,9 @@ git fetch --tags
 # This suppress an error occurred when the repository is a complete one.
 git fetch --prune --unshallow || true
 
-latest_tag=''
+last_tag=$(python3 /get_last_tag.py /github/workspace)
 
-if [ "${INPUT_SEMVER_ONLY}" = 'false' ]; then
-  # Get a actual latest tag.
-  # If no tags found, supress an error. In such case stderr will be not stored in latest_tag variable so no additional logic is needed.
-  latest_tag=$(git describe --abbrev=0 --tags || true)
-else
-  # Get a latest tag in the shape of semver.
-  for ref in $(git for-each-ref --sort=-creatordate --format '%(refname)' refs/tags); do
-    tag="${ref#refs/tags/}"
-    if echo "${tag}" | grep -Eq '^v?([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$'; then
-      latest_tag="${tag}"
-      break
-    fi
-  done
-fi
+release=$(pysemver bump "${INPUT_VERSION_FRAGMENT}" "${last_tag}")
+echo "::debug::Bumped version: $release"
 
-if [ "${latest_tag}" = '' ] && [ "${INPUT_WITH_INITIAL_VERSION}" = 'true' ]; then
-  latest_tag="${INPUT_INITIAL_VERSION}"
-fi
-
-echo "::set-output name=tag::${latest_tag}"
+echo "version_bumped=${release}" >> $GITHUB_OUTPUT
