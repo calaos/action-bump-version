5f032f9d4be28d78c858e3db1ad1bb9c9df14bbc
Raoul Hecky <raoul.hecky@gmail.com>
2023-12-05 10:51:06 +0100

    Fix

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM alpine:latest
 
-RUN apk add --update --no-cache git grep python3 py3-semver py3-gitpython
+RUN apk add --update --no-cache git grep python3 py3-semver
 
 COPY entrypoint.sh /entrypoint.sh
-COPY get_last_tag.py /get_last_tag.py
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,35 @@
 
 set -e
 
-git config --global --add safe.directory /github/workspace
+repo='/github/workspace'
+
+git config --global --add safe.directory $repo
 
 git fetch --tags
 # This suppress an error occurred when the repository is a complete one.
 git fetch --prune --unshallow || true
 
-last_tag=$(python3 /get_last_tag.py /github/workspace)
+last_tag='0.0.0'
+
+#get last tag in the shape of semver
+cd $repo
+for ref in $(git for-each-ref --sort=-creatordate --format '%(refname)' refs/tags); do
+    tag="${ref#refs/tags/}"
+
+    # skip if tag starts with a letter
+    case "$tag" in
+        [a-zA-Z]*)
+            continue
+            ;;
+    esac
+
+    last_tag="${tag}"
+    break
+done
+
+echo "::notice::Last version: $last_tag"
 
 release=$(pysemver bump "${INPUT_VERSION_FRAGMENT}" "${last_tag}")
-echo "::debug::Bumped version: $release"
+echo "::notice::Bumped version: $release"
 
 echo "version_bumped=${release}" >> $GITHUB_OUTPUT
